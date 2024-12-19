cba04210fb377bc487dc5e9b43282577412425fd
Sanket Mistry <sanket.mern.dev@gmail.com>
2024-12-20 03:44:01 +0530

    Main layout & Left sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import AuthCallbackPage from './pages/authCallback/AuthCallbackPage.tsx';
+import MainLayout from './layout/MainLayout.tsx';
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage />} />
         <Route
           path='/sso-callback'
           element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={'/auth-callback'} />}
         />
         <Route path='/auth-callback' element={<AuthCallbackPage />} />
-        <Route />
+        <Route element={<MainLayout />}>
+					<Route path='/' element={<HomePage />} />
+				</Route>
       </Routes>
       {/* <header>
       <SignedOut>
